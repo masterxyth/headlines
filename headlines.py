8f26522e537b3546e9e23b5e6f25642d7ccc96bc
masterxyth <rizwanq@sfu.ca>
2017-05-28 12:27:11 -0700

    completed with CSS and cookies

--- headlines.py
+++ headlines.py
@@ -1,26 +1,95 @@
+import datetime
+import json
+
+
 import feedparser
+import requests
+import urllib
+import urllib2
+
 from flask import Flask
+from flask import make_response
 from flask import render_template
 from flask import request
 
 app = Flask(__name__)
+
+# Adding default variables so we always have data
+
+DEFAULTS = {'publication':'bbc',
+            'city': 'London,UK',
+            'currency_from': 'GBP',
+            'currency_to': 'USD'}
+
+# List of Feeds
 
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'cnn': 'http://rss.cnn.com/rss/edition.rss',
              'fox': 'http://feeds.foxnews.com/foxnews/latest',
              'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
-@app.route("/")
+# Global APIs
+WEATHER_URL = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=754e794a991cfb7d6c324f4c33998563'
 
-def get_news(publication='bbc'):
-    query = request.args.get("publication")
+CURRENCY_URL = "https://openexchangerates.org//api/latest.json?app_id=024c7fe3bcf14390a27c8540a42f1000"
+
+@app.route("/")
+def home():
+    # get customized headlines, based on user input or default
+    publication = get_value_with_fallback('publication')
+    articles = get_news(publication)
+    # get customized weather based on user input or default
+    city = get_value_with_fallback('city')
+    weather = get_weather(city)
+
+    #customized currency based on user input or default
+    currency_from = get_value_with_fallback('currency_from')
+    currency_to = get_value_with_fallback('currency_to')
+    rate, currencies = get_rates(currency_from, currency_to)
+
+    # save cookies and return template
+
+    response = make_response(render_template('home.html', articles=articles, weather=weather, currency_from=currency_from, currency_to=currency_to, rate=rate, currencies=sorted(currencies)))
+    expires = datetime.datetime.now()+datetime.timedelta(days=365)
+    response.set_cookie('publication', publication, expires=expires)
+    response.set_cookie('city', city, expires=expires)
+    response.set_cookie('currency_from', currency_from, expires=expires)
+    response.set_cookie('currency_to', currency_to, expires=expires)
+    return response
+
+def get_news(query):
     if not query or query.lower() not in RSS_FEEDS:
-            publication = "bbc"
+        publication = DEFAULTS["publication"]
     else:
         publication = query.lower()
-
     feed = feedparser.parse(RSS_FEEDS[publication])
-    return render_template("home.html", articles=feed['entries'])
+    return feed['entries']
+
+def get_weather(query):
+    query = urllib.quote(query)
+
+    url = WEATHER_URL.format(query)
+    data = urllib2.urlopen(url).read()
+    parsed = json.loads(data)
+    weather = None
+    if parsed.get('weather'):
+        weather = {'description':parsed['weather'][0]['description'],'temperature':parsed['main']['temp'],'city':parsed['name']}
+    return weather
+
+def get_rates(frm, to):
+    all_currency = urllib2.urlopen(CURRENCY_URL).read()
+    parsed = json.loads(all_currency).get('rates')
+    frm_rate = parsed.get(frm.upper())
+    to_rate = parsed.get(to.upper())
+    return (to_rate/frm_rate, parsed.keys())
+
+def get_value_with_fallback(key):
+    if request.args.get(key):
+        return request.args.get(key)
+    if request.cookies.get(key):
+        return request.cookies.get(key)
+    return DEFAULTS[key]
+
 
 if __name__ == '__main__':
   app.run(port=5000, debug=True)
